ed5c7d6c6416772078cfcd8cec17650aea416f7c
akhil2006 <68735728+akhil2006@users.noreply.github.com>
2021-04-08 16:38:56 +0530

    Add files via upload

---
+++ carRacingStage2.5-master/rank.js
@@ -0,0 +1,10 @@
+class Rank{
+    constructor(){
+
+    }
+    display(){
+        var message= createElement('h3')
+        message.position(displayWidth/2 -100 ,displayHeight/2)
+   message.html("player rank : " +player.rank)
+    }
+}
